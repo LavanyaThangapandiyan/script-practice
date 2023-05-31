8b2f401ee0d19bfb597ef8531c88fafc76815316
LavanyaThangapandiyan <128019444+LavanyaThangapandiyan@users.noreply.github.com>
2023-05-31 20:31:58 +0530

    Add files via upload

---
+++ javaScript/statement&declaration/script.js
@@ -0,0 +1,22 @@
+/* let password= false
+if(password)
+console.log("You are Logged In");
+else
+console.log("Thank you");*/
+
+/*let age=50
+let gender='Male';
+if(age>45&& gender=='Male')
+console.log("You are qualified");
+else
+console.log("Not Qualified");*/
+
+/*//max of two number
+let number1=20
+let number2=30
+let max
+/*if(number1>number2)
+max=number1;
+else
+max=number1>number2?number1:number2;
+console.log(max);*/
